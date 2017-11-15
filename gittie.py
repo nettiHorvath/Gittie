d74ecfbc99da33c82e07f952a5e2bb21dc450a86
Dominik Hug <h.dom90@gmail.com>
2017-11-15 09:37:47 +0100

    introduce modified.

--- gittie.py
+++ gittie.py
@@ -1,12 +1,9 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm NOT Gittie!")
 
-<<<<<<< HEAD
 
 def add(a, b):
     return a + b
-=======
 def joke():
     print("Mi az cukros és szobahőmérsekleten köt?")
     print("Nagymama.")
->>>>>>> da6963f1a2a670fcb1293870c8ae604d29bcda93
